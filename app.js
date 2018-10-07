44fe9188a638eb58aaee3dd9ddb8f4bd0b36ba67
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2018-10-07 17:53:12 +0000

    allows app to be called from preview pane

--- app.js
+++ app.js
@@ -38,4 +38,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+  //call this app from https://<workspace name>-<user name>.c9users.io
+});
+
 module.exports = app;
